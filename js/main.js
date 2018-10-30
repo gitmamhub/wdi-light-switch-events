5f216653a2db89a8497468b34fca96576058a765
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-30 17:09:30 -0400

    switch is working

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 
 
+document.addEventListener('DOMContentLoaded', function() {
+
+  var click_sw = document.querySelector('.switch.on');
+
+
+ click_sw.addEventListener('click', function(event) {
+
+    var click_sw = document.querySelector('.switch');
+    var click_sw1 = document.querySelector('body');
+    var click_sw2 = document.querySelector('.status');
+
+    click_sw.className = "switch off";
+
+    click_sw1.className = "dark";
+    click_sw2.innerText  = "I can see YOU !!! "
+
+    event.preventDefault();
+    alert("Hey, who turned off the lights?");
+
+
+  });
+  //
+});
